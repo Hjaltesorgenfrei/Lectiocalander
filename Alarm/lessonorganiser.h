076a73a469da64180cb90e9b27bd876aa2ef9c74
Hjalte Sorgenfrei Mac Dalland <minihjalte@gmail.com>
2016-05-21 23:04:47 +0200

    Actually removed the files now

--- Alarm/lessonorganiser.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-#include "datestruct.h"
-#include "dateformater.h"
-
-std::vector<Datestruct> slessonorgansiser(std::string htmlinput);
--- Alarm/stringtodatestruct.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include "datestruct.h"
-#include <vector>
-#include <string>
-
-std::vector<Datestruct> stringtodatestruct(std::vector<std::string> unformattedlessons, char *formatter);
